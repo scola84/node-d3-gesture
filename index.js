233f961a05264ad505d065052bb5937700d0cc59
M.C. van den Boogaart <scola@kvdb.name>
2017-05-12 10:50:23 +0200

    Set input class

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import { selection } from 'd3';
 import Hammer from 'hammerjs';
 import propagating from 'propagating-hammerjs';
 
-selection.prototype.gesture = function swipe(options) {
+selection.prototype.gesture = function gesture(options) {
+  options = Object.assign({}, options, {
+    inputClass: Hammer.TouchMouseInput
+  });
+
   return propagating(new Hammer(this.node(), options));
 };
